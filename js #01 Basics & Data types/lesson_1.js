d42c173c8677c569c88c5e5045df225d9f2229a6
grandson1337dev <stanislav97dev@gmail.com>
2026-03-01 20:43:48 +0200

    Add files via upload

---
+++ js #01 Basics & Data types/lesson_1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let age = 27;
+const myName = 'Stanislav';
+console.log(age, myName);
+
+
+// --- ТИПИ ДАНИХ ---
+
+// 1. STRING
+const myString1 = 'Hello!';
+const myString2 = "World!";
+// console.log(typeof myString1);
+
+// 2. NUMBER
+const x = 25;
+const y = 0.25;
+// console.log(typeof y);
+
+// 3. BigInt
+const myBigNumber = 32981639812638216386261893621n;
+const myAnotherBigNumber = BigInt('31983612836218936');
+// console.log(typeof myBigNumber);
+
+// 4. BOOLEAN
+let hasDog = false; // true or 1
+// console.log(typeof hasDog);
+
+// 5. NULL
+let salary = null;
+// console.log(typeof salary); // object
+
+// 6. UNDEFINED
+let something;
+// console.log(typeof something);
+
+// 7. SYMBOL
+// 8. OBJECT
